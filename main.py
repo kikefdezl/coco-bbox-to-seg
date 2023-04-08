02bff93544d709973dfffb915fb00760011f9e35
Kike <enriquelagui@outlook.com>
2023-04-08 13:11:14 +0200

    Add settings.py & remove cv2 dependency

--- main.py
+++ main.py
@@ -1,25 +1,29 @@
-from pathlib import Path
 from argparse import ArgumentParser
+from pathlib import Path
 
-from segment_anything import build_sam, SamPredictor
 import numpy as np
+from segment_anything import build_sam, SamPredictor
 from skimage import measure
-import cv2
+from skimage.io import imread
 from tqdm import tqdm
 
+import settings
 from coco import CocoDataset
 from utils import load_json, save_json
 
-CKPT_PATH = Path("ckpt/sam_vit_h_4b8939.pth")
-IMAGE_EXTS = {".jpg", ".jpeg", ".png", ".tif", ".tiff", ".gif"}
+CKPT_PATH = Path(settings.CKPT_PATH)
+IMAGE_EXTS = settings.IMAGE_EXTS
+DEFAULT_OUTPUT_FILENAME = settings.DEFAULT_OUTPUT_FILENAME
+
 
 def parse_args():
     parser = ArgumentParser()
-    parser.add_argument("coco_json")
-    parser.add_argument("images")
-    parser.add_argument("output")
+    parser.add_argument("coco_json", help="Path to the COCO JSON file.")
+    parser.add_argument("images", help="Path to the folder with the images.")
+    parser.add_argument("output", help="Output path.")
     args = parser.parse_args()
     return args
+
 
 def main():
     args = parse_args()
@@ -27,26 +31,34 @@ def main():
     coco_dataset = CocoDataset(**data)
 
     output_filename = Path(args.output)
-    assert output_filename.suffix.lower() == ".json"
+    if output_filename.suffix.lower() != ".json":
+        output_filename = output_filename / DEFAULT_OUTPUT_FILENAME
 
     predictor = SamPredictor(build_sam(checkpoint=CKPT_PATH.as_posix()))
 
     image_dir = Path(args.images)
 
-    for coco_image in tqdm(coco_dataset.images, "Images", position=0, leave=False):
+    for coco_image in tqdm(coco_dataset.images, "Images", position=0,
+                           leave=False):
         image_filepath = image_dir / coco_image.file_name
-        annotations = [a for a in coco_dataset.annotations if a.image_id == coco_image.id]
-        image = cv2.imread(image_filepath.as_posix())
-        predictor.set_image(image)
-        for annotation in tqdm(annotations, "Annotations", position=1, leave=False):
-            bbox = np.array(annotation.bbox)
+        annotations = [a for a in coco_dataset.annotations if
+                       a.image_id == coco_image.id]
+        image = imread(image_filepath.as_posix())
+        predictor.set_image(image, image_format="BGR")
+        for annotation in tqdm(annotations, "Annotations", position=1,
+                               leave=False):
+            bbox = np.array([
+                annotation.bbox[0],
+                annotation.bbox[1],
+                annotation.bbox[0] + annotation.bbox[2],
+                annotation.bbox[1] + annotation.bbox[3]
+            ])
             mask = predictor.predict(box=bbox, multimask_output=False)[0][0]
-            contours = measure.find_contours(mask, 0.5)
+            contours = measure.find_contours(mask.T, 0.5)
             if not contours:
                 continue
             contour = contours[0]
             annotation.segmentation = [contour.flatten().tolist()]
-            x=0
 
     save_json(coco_dataset.dict(), output_filename)
     print(f"Saved auto-segmented JSON at: {output_filename.as_posix()}")
