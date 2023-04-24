ad0bb44f1ecb0de59879f1088054a7589e80b2e9
Kike <enriquelagui@outlook.com>
2023-04-24 13:11:11 +0200

    Add progress bar for download checkpoint function

--- bbox_to_seg/settings.py
+++ bbox_to_seg/settings.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 IMAGE_EXTS = {".jpg", ".jpeg", ".png", ".tif", ".tiff", ".gif"}
 DEFAULT_OUTPUT_FILENAME = "segmented.json"
 MODEL = "VIT_H"
@@ -7,3 +9,4 @@ MODELS = {
         "DOWNLOAD_LINK": "https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth",
     }
 }
+PACKAGE_ROOT = Path(__file__).parent.parent.as_posix()
--- bbox_to_seg/utils.py
+++ bbox_to_seg/utils.py
@@ -4,15 +4,24 @@ import json
 from pathlib import Path
 
 import requests
+from tqdm import tqdm
 
-from .settings import MODEL, MODELS
+from .settings import MODEL, MODELS, PACKAGE_ROOT
 
 
 def download_ckpt():
-    print("Downloading model checkpoint... ", end="")
-    r = requests.get(MODELS[MODEL]["DOWNLOAD_LINK"], allow_redirects=True)
-    open(MODELS[MODEL]["CKPT_PATH"], "wb").write(r.content)
-    print("done.")
+    print("Downloading model checkpoint... ")
+    r = requests.get(
+        MODELS[MODEL]["DOWNLOAD_LINK"], allow_redirects=True, stream=True
+    )
+    total = int(r.headers.get("content-length", 0))
+    fname = (Path(PACKAGE_ROOT) / MODELS[MODEL]["CKPT_PATH"]).as_posix()
+    with open(fname, "wb") as file, tqdm(
+        desc=fname, total=total, unit="iB", unit_scale=True, unit_divisor=1024
+    ) as bar:
+        for data in r.iter_content(chunk_size=1024):
+            size = file.write(data)
+            bar.update(size)
 
 
 def load_json(filepath: str | Path) -> dict:
